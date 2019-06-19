6f04e34d7e1b7f117d419272aa4e11585e941808
Kenneth <kenny.laprelle@gmail.com>
2019-06-19 16:53:04 -0700

    git ignore

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use('/', express.static(__dirname + '/./public/'));
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
-const server = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+app.listen(process.env.PORT || 7000, () => {
+  console.log('Express running');
 });
